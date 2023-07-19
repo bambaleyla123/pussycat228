19f521858b35a69ce50e3c51aeac8bbfb8ebee8c
Семен Кулешов <semenkulesov@MacBook-Air-Semen.local>
2023-07-19 22:32:58 +0200

    slider

--- src/components/InfHSlider.jsx
+++ src/components/InfHSlider.jsx
@@ -7,7 +7,7 @@ const InfHSlider = ({ images }) => {
         <li key={key} className="snap-start min-w-[80vw] md:min-w-[24rem]">
           <img
             src={el}
-            className="aspect-[2/3] object-cover w-[80vw] md:w-96"
+            className="w-[80vw] md:w-96"
           ></img>
         </li>
       ))}
