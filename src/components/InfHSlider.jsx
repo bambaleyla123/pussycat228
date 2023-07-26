dceeea979c6ff046f2910ee64ca22985215e39a9
Семен Кулешов <semenkulesov@MacBook-Air-Semen.local>
2023-07-26 16:08:36 +0200

    «src»

--- src/components/InfHSlider.jsx
+++ src/components/InfHSlider.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
 
 const InfHSlider = ({ images }) => {
-  return (
-    <ul className="flex overflow-x-auto snap-x">
-      {images.map((el, key) => (
-        <li key={key} className="snap-start min-w-[80vw] md:min-w-[24rem]">
-          <img
-            src={el}
-            className="w-[80vw] md:w-96"
-          ></img>
-        </li>
-      ))}
-    </ul>
-  );
+    const [imagesList, setImagesList] = useState(images);
+    const elementRef = useRef();
+
+    // -----------------
+
+    // -----------------
+
+    return (
+        <ul className="flex overflow-x-auto snap-x">
+            {imagesList.map((el, key) => (
+                <li
+                    key={key}
+                    className="snap-start min-w-[80vw] md:min-w-[24rem]"
+                >
+                    <img
+                        src={el}
+                        className="aspect-[2/3] object-cover w-[80vw] md:w-96"
+                    ></img>
+                </li>
+            ))}
+            <span ref={elementRef} className="w-[1px]">
+                .
+            </span>
+        </ul>
+    );
 };
 
 export default InfHSlider;
